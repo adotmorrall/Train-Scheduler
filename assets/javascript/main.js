1d233053a117696435eda28bdb10737be160f3fe
Andre <andre.morrall@icloud.com>
2019-12-10 16:55:21 -0600

    Added Firebase configuration

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+// Firebase Configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA5rHLTbYBR4RWOpjRqQzbxKRIcDpSqaUE",
+    authDomain: "andre-firebase.firebaseapp.com",
+    databaseURL: "https://andre-firebase.firebaseio.com",
+    projectId: "andre-firebase",
+    storageBucket: "andre-firebase.appspot.com",
+    messagingSenderId: "666199349670",
+    appId: "1:666199349670:web:86a34722305eca2dd11767"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference to database
+
+var database = firebase.database();
